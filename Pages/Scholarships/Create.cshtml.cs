341ff7280b41cd93ed3c3daa9c904c96b8f8f543
gurnoora21 <gurnooraujla10@gmail.com>
2020-03-16 18:31:52 -0600

    added an upload button and file browser to scholarship creation

--- Pages/Scholarships/Create.cshtml.cs
+++ Pages/Scholarships/Create.cshtml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -40,6 +42,21 @@ namespace SENG300Scholarships.Pages.Scholarships
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
+        }
+
+        
+
+    [BindProperty]
+            public BufferedSingleFileUploadDb FileUpload { get; set; }
+
+    
+
+
+        public class BufferedSingleFileUploadDb
+        {
+            [Required]
+            [Display(Name = "File")]
+            public IFormFile FormFile { get; set; }
         }
     }
 }
--- Pages/Users/Index.cshtml.cs
+++ Pages/Users/Index.cshtml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -19,11 +21,15 @@ namespace SENG300Scholarships
             _context = context;
         }
 
-        public IList<User> User { get;set; }
+        public IList<User> User { get; set; }
 
         public async Task OnGetAsync()
         {
             User = await _context.Users.ToListAsync();
         }
+
+
+
+
     }
 }
